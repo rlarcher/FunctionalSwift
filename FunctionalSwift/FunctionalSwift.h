3bfeba801de97cb668f6b68d594248e659326d06
Ryan Archer <rlarcher@andrew.cmu.edu>
2015-08-13 16:51:53 -0400

    Initial Commit

---
+++ FunctionalSwift/FunctionalSwift.h
@@ -0,0 +1,19 @@
+//
+//  FunctionalSwift.h
+//  FunctionalSwift
+//
+//  Created by Ryan Archer on 8/13/15.
+//  Copyright (c) 2015 RyanArcher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FunctionalSwift.
+FOUNDATION_EXPORT double FunctionalSwiftVersionNumber;
+
+//! Project version string for FunctionalSwift.
+FOUNDATION_EXPORT const unsigned char FunctionalSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FunctionalSwift/PublicHeader.h>
+
+
